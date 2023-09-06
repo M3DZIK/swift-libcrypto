37971e6760e84e8c4cb3bd3a1ed6b10d0b8a2264
M3DZIK <me@medzik.dev>
2023-09-06 22:31:51 +0200

    Fix tests

--- Tests/LibCryptoTests/Argon2.swift
+++ Tests/LibCryptoTests/Argon2.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import LibCrypto
 
 final class Argon2Tests: XCTestCase {
-    func hashExample() throws {
+    func testHash() throws {
         let argon2 = Argon2(parallelism: 1, memory: 65536, iterations: 1, hashLength: 32)
 
         let result = try! argon2.hashPasswordString(password: "secret password", salt:  Salt.generate(length: 16))
@@ -10,7 +10,7 @@ final class Argon2Tests: XCTestCase {
         XCTAssert(try! Argon2.verifyHashString(password: "secret password", hash: result.encodedString(), type: .id))
     }
 
-    func validHashExample() throws {
+    func testValidHash() throws {
         let hash = "$argon2id$v=19$m=15360,t=2,p=1$bWVkemlrQGR1Y2suY29t$n7wCfzdczbjclMnpvw+t/4D+mCcCFUU+hm6Z85k81PQ"
         XCTAssert(try! Argon2.verifyHashString(password: "medzik@duck.com", hash: hash, type: .id))
     }
